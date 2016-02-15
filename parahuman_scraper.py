3c1262f4026cf2556f5d3266336436a7a5614582
Matthew Stratton <development@mmstratton.com>
2016-02-15 00:36:16 -0500

    switching to lxml for more powerful css selectors

--- parahuman_scraper.py
+++ parahuman_scraper.py
@@ -1,6 +1,9 @@
-from BeautifulSoup import BeautifulSoup
+import lxml.html
+from lxml.cssselect import CSSSelector
 from collections import OrderedDict
-import urllib2, os, pickle
+from lxml.etree import tostring
+from itertools import chain
+import urllib2, pickle
 
 class Chapter:
     title_class = 'entry-title'
@@ -8,47 +11,46 @@ class Chapter:
     
     def __init__(self, url):
         self.url = url
-        self.soup = None
+        self.tree = None
         self.title = None
         self.text_markup = None
 
     def __getstate__(self):
         odict = self.__dict__.copy()
-        if 'soup' in odict:
-            del odict['soup']
+        if 'tree' in odict:
+            del odict['tree']
         return odict
     
     def load_dom(self, refresh=False):
-        if self.soup is None or refresh:
-            print 'missed the boat'
+        if self.tree is None or refresh:
             response = urllib2.urlopen(self.url.encode('utf-8'))
-            self.soup = BeautifulSoup(response.read().decode('utf-8', 'ignore'))
+            self.tree = lxml.html.fromstring(response.read().decode('utf-8', 'ignore'))
             response.close()
-        return self.soup
+        return self.tree 
 
-    def decompose(self):
-        if self.soup is not None:
-            self.soup.decompose()
-            self.soup = Nonet_markup = f.read()
-        return self        
+    def stringify_children(self, node):
+        parts = ([node.text] +
+                list(chain(*([c.text, tostring(c), c.tail] for c in node.getchildren()))) +
+                [node.tail])
+        # filter removes possible Nones in texts and tails
+        return ''.join(filter(None, parts))     
         
     def get_title(self, refresh=False):
         if self.title is None or refresh:
-            header_title = self.load_dom().find('h1', {'class': Chapter.title_class})
-            if header_title is not None:
-                if header_title.find('a') is not None and header_title.find('a') is not None:
-                    self.title = str(header_title.find('a').contents[0])
-                elif header_title.contents[0] is not None:
-                    self.title = str(header_title.contents[0])
+            sel = CSSSelector('h1.entry-title a, h1.entry-title')
+            for t in sel(self.load_dom()):
+                self.title = t.text
     
         return self.title
     
+    #need to figure out how to prun next/prev chapter nodes when scrping body
     def get_text(self, refresh=False):
         if self.text_markup is None or refresh:
-            dom = self.load_dom().find('div', {'class': Chapter.chapter_class}).findAll('p')
-            del dom[0]
-            del dom[len(dom)-1]
-            self.text_markup = ''.join(p.text for p in dom)
+            sel = CSSSelector('div.entry-content p')
+
+            match = sel(self.load_dom())
+
+            self.text_markup = ''.join(self.stringify_children(p) for p in match)
 
         return self.text_markup
         
@@ -58,13 +60,13 @@ class Contents:
     def __init__(self, url, cache_location='./cache/parahumans'):
         self.url = url
         self.cache_location = cache_location
-        self.soup = None
+        self.tree = None
         self.chapters = OrderedDict()
 
     def __getstate__(self):
         odict = self.__dict__.copy()
-        if 'soup' in odict:
-            del odict['soup']
+        if 'tree' in odict:
+            del odict['tree']
         return odict
 
     @classmethod
@@ -73,16 +75,12 @@ class Contents:
             return pickle.load(f)
         
     def load_dom(self, refresh=False):
-        if self.soup is None or refresh:
+        if self.tree is None or refresh:
+            print 'loading webpage ' + self.url
             response = urllib2.urlopen(self.url.encode('utf-8'))
-            self.soup = BeautifulSoup(response.read().decode('utf-8', 'ignore'))
+            self.tree = lxml.html.fromstring(response.read().decode('utf-8', 'ignore'))
             response.close()
-        return self.soup
-
-    def decompose(self):
-        if self.soup is not None:
-            self.soup.decompose()
-            self.soup = None
+        return self.tree
 
     def cache(self):
         with open(self.cache_location, 'wb') as cache:
@@ -90,25 +88,29 @@ class Contents:
 
     def get_chapters(self, refresh=False):
         if len(self.chapters) == 0 or refresh:
-            for link in self.load_dom().find('div', {'class': Contents.toc_entry_class}).findAll('a'):
-                href = link['href']
+            sel = CSSSelector('div.entry-content a:not([href*="share"])')
+            for link in sel(self.load_dom()):
+            #for link in self.load_dom().find('div', {'class': Contents.toc_entry_class}).findAll('a'):
+                href = link.get('href')
                 if not href.startswith('https://'):
                     href = 'https://' + href
 
-                if href not in self.chapters and 'facebook' not in href and 'twitter' not in href:
+                if href not in self.chapters:
+                #if href not in self.chapters and 'facebook' not in href and 'twitter' not in href:
                     self.chapters[href] = Chapter(href)
 
         return self.chapters.values()
         
 if __name__ == '__main__':
-    #toc = Contents('https://parahumans.wordpress.com/table-of-contents/')
-    #toc.chapters['https://parahumans.wordpress.com/2012/06/30/plague-12-2/'] = Chapter('https://parahumans.wordpress.com/2012/06/30/plague-12-2/')
-    #toc.chapters['https://parahumans.wordpress.com/2012/07/26/interlude-12%C2%BD/'] = Chapter('https://parahumans.wordpress.com/2012/07/26/interlude-12%C2%BD/')
+    toc = Contents('https://parahumans.wordpress.com/table-of-contents/')
+    toc.chapters['https://parahumans.wordpress.com/2013/03/05/scourge-19-5/'] = Chapter('https://parahumans.wordpress.com/2013/03/05/scourge-19-5/')
+    #toc.chapters['https://parahumans.wordpress.com/category/stories-arcs-1-10/arc-5-hive/5-06/'] = Chapter('https://parahumans.wordpress.com/category/stories-arcs-1-10/arc-5-hive/5-06/')
 
-    toc = Contents.restore()
+    #toc = Contents.restore()
 
     for chapter in toc.get_chapters():
+        print chapter.url
         print chapter.get_title()
-        #print chapter.get_text()
+        print chapter.get_text()
 
     toc.cache()
