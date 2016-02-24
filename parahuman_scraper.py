fff48b6f9eac4d6afc0efd5572e63bf274441966
Matthew Stratton <development@mmstratton.com>
2016-02-24 00:27:10 -0500

    got a rough epub going on, just need to tie it all together and spend like 30 hours getting css to do what I want

--- parahuman_scraper.py
+++ parahuman_scraper.py
@@ -3,11 +3,14 @@ from lxml.cssselect import CSSSelector
 from collections import OrderedDict
 from lxml.etree import tostring
 from itertools import chain
-import urllib2, pickle
+from urllib.request import urlopen
+from ebooklib import epub
+import pickle, uuid, re
 
 class Chapter:
     title_class = 'entry-title'
     chapter_class = 'entry-content'
+    section_regex = '^(\w*) \d.*$'
     
     def __init__(self, url):
         self.url = url
@@ -23,7 +26,7 @@ class Chapter:
     
     def load_dom(self, refresh=False):
         if self.tree is None or refresh:
-            response = urllib2.urlopen(self.url.encode('utf-8'))
+            response = urlopen(self.url)
             self.tree = lxml.html.fromstring(response.read().decode('utf-8', 'ignore'))
             response.close()
         return self.tree 
@@ -38,21 +41,33 @@ class Chapter:
     def get_title(self, refresh=False):
         if self.title is None or refresh:
             sel = CSSSelector('h1.entry-title a, h1.entry-title')
-            for t in sel(self.load_dom()):
+            for t in sel(self.load_dom(refresh)):
                 self.title = t.text
     
         return self.title
-    
+
     #need to figure out how to prun next/prev chapter nodes when scrping body
     def get_text(self, refresh=False):
         if self.text_markup is None or refresh:
             sel = CSSSelector('div.entry-content p')
 
-            match = sel(self.load_dom())
+            match = sel(self.load_dom(refresh))
 
-            self.text_markup = ''.join(self.stringify_children(p) for p in match)
+            self.text_markup = ''.join(tostring(p, encoding='unicode', pretty_print=True) for p in match if len(p.getchildren()) == 0)
 
         return self.text_markup
+
+    def get_epub_section(self, refresh=False):
+        return re.match(Chapter.section_regex, self.get_title(refresh)).group(1)
+
+    def get_epub_filename(self, refresh=False):
+        return self.get_title(refresh).replace(' ', '_') + '.xhtml'
+
+    def to_epub(self, css, refresh=False):
+        epub_chapter = epub.EpubHtml(title=self.get_title(refresh), file_name=self.get_epub_filename(refresh), lang='hr')
+        epub_chapter.content='<html><body><h1>'+self.get_title(refresh)+'</h1>'+self.get_text(refresh)+'</body></html>'
+        epub_chapter.add_item(css)
+        return epub_chapter
         
 class Contents:
     toc_entry_class = 'entry-content'
@@ -76,8 +91,8 @@ class Contents:
         
     def load_dom(self, refresh=False):
         if self.tree is None or refresh:
-            print 'loading webpage ' + self.url
-            response = urllib2.urlopen(self.url.encode('utf-8'))
+            print('loading webpage ' + self.url)
+            response = urlopen(self.url)
             self.tree = lxml.html.fromstring(response.read().decode('utf-8', 'ignore'))
             response.close()
         return self.tree
@@ -90,27 +105,72 @@ class Contents:
         if len(self.chapters) == 0 or refresh:
             sel = CSSSelector('div.entry-content a:not([href*="share"])')
             for link in sel(self.load_dom()):
-            #for link in self.load_dom().find('div', {'class': Contents.toc_entry_class}).findAll('a'):
                 href = link.get('href')
                 if not href.startswith('https://'):
                     href = 'https://' + href
 
                 if href not in self.chapters:
-                #if href not in self.chapters and 'facebook' not in href and 'twitter' not in href:
                     self.chapters[href] = Chapter(href)
 
-        return self.chapters.values()
+        return self.chapters.values()        
+
+    def to_epub(self, book, css):
+        sections = OrderedDict()
+        chapters = []
+
+        for chapter in self.get_chapters():
+            epub_chapter = chapter.to_epub(css)
+            chapters.append(epub_chapter)
+            book.add_item(epub_chapter)
+
+            print(chapter.get_epub_section())
+
+            if chapter.get_epub_section() not in sections:
+                sections[chapter.get_epub_section()] = []
+            sections[chapter.get_epub_section()].append(epub_chapter)
+
+        book.toc = [(epub.Section(section), tuple(chapters)) for section, chapters in sections.items()]
+        
+        # basic spine
+        book.spine = ['nav'] + chapters
+        return book
         
 if __name__ == '__main__':
+    book = epub.EpubBook()
+
+    # set metadata
+    book.set_identifier(str(uuid.uuid4()))
+    book.set_title('Parahumans - Worm')
+    book.set_language('en')
+
+    book.add_author('Wildbow')
+
     toc = Contents('https://parahumans.wordpress.com/table-of-contents/')
     toc.chapters['https://parahumans.wordpress.com/2013/03/05/scourge-19-5/'] = Chapter('https://parahumans.wordpress.com/2013/03/05/scourge-19-5/')
-    #toc.chapters['https://parahumans.wordpress.com/category/stories-arcs-1-10/arc-5-hive/5-06/'] = Chapter('https://parahumans.wordpress.com/category/stories-arcs-1-10/arc-5-hive/5-06/')
+    toc.chapters['https://parahumans.wordpress.com/category/stories-arcs-1-10/arc-3-agitation/3-05/'] = Chapter('https://parahumans.wordpress.com/category/stories-arcs-1-10/arc-3-agitation/3-05/')
 
-    #toc = Contents.restore()
+    # define CSS style
+    toc_css = open('toc.css', 'r')
+    style = toc_css.read()
+    toc_css.close()
 
-    for chapter in toc.get_chapters():
-        print chapter.url
-        print chapter.get_title()
-        print chapter.get_text()
+    nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
 
-    toc.cache()
+    kindle_css = open('kindle.css', 'r')
+    style = kindle_css.read()
+    kindle_css.close()
+
+    default_css = epub.EpubItem(uid="style_default", file_name="style/default.css", media_type="text/css", content=style)
+
+    # add CSS file
+    book.add_item(nav_css)
+    book.add_item(default_css)    
+
+    book = toc.to_epub(book, default_css)
+
+    # add default NCX and Nav file
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
+
+    # write to the file
+    epub.write_epub('test.epub', book, {})
