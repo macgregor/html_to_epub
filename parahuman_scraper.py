012d2ba8ccef77ae1c2f5f4d7f5c82f390218b17
Matthew Stratton <development@mmstratton.com>
2016-02-24 21:16:11 -0500

    fixing error with unicode characters in url removing refresh parameters finishing refactoring to Book class

--- parahuman_scraper.py
+++ parahuman_scraper.py
@@ -1,11 +1,12 @@
 import lxml.html
 from lxml.cssselect import CSSSelector
-from collections import OrderedDict
 from lxml.etree import tostring
-from itertools import chain
+from collections import OrderedDict
 from urllib.request import urlopen
+from urllib import parse
 from ebooklib import epub
-import pickle, uuid, re
+from tqdm import tqdm
+import pickle, uuid, re, os
 
 class Chapter:
     title_class = 'entry-title'
@@ -13,6 +14,11 @@ class Chapter:
     section_regex = '^(\w*) \d.*$'
     
     def __init__(self, url):
+        url = parse.urlsplit(url)
+        url = list(url)
+        url[2] = parse.quote(url[2])
+        url = parse.urlunsplit(url)
+
         self.url = url
         self.tree = None
         self.title = None
@@ -20,123 +26,109 @@ class Chapter:
 
     def __getstate__(self):
         odict = self.__dict__.copy()
-        if 'tree' in odict:
-            del odict['tree']
+        odict['tree'] = None
         return odict
-    
-    def load_dom(self, refresh=False):
-        if self.tree is None or refresh:
-            response = urlopen(self.url)
-            self.tree = lxml.html.fromstring(response.read().decode('utf-8', 'ignore'))
-            response.close()
-        return self.tree 
 
-    def stringify_children(self, node):
-        parts = ([node.text] +
-                list(chain(*([c.text, tostring(c), c.tail] for c in node.getchildren()))) +
-                [node.tail])
-        # filter removes possible Nones in texts and tails
-        return ''.join(filter(None, parts))     
+    def load_dom(self):
+        response = urlopen(self.url)
+        self.tree = lxml.html.fromstring(response.read().decode('utf-8', 'ignore'))
+        response.close()
+
+        self.get_title()
+        self.get_text()
+
+        return self.tree   
         
-    def get_title(self, refresh=False):
-        if self.title is None or refresh:
+    def get_title(self):
+        if self.tree is not None:
             sel = CSSSelector('h1.entry-title a, h1.entry-title')
-            for t in sel(self.load_dom(refresh)):
+            for t in sel(self.tree):
                 self.title = t.text
     
         return self.title
 
-    #need to figure out how to prun next/prev chapter nodes when scrping body
-    def get_text(self, refresh=False):
-        if self.text_markup is None or refresh:
+    def get_text(self):
+        if self.tree is not None:
             sel = CSSSelector('div.entry-content p')
-
-            match = sel(self.load_dom(refresh))
-
+            match = sel(self.tree)
             self.text_markup = ''.join(tostring(p, encoding='unicode', pretty_print=True) for p in match if len(p.getchildren()) == 0)
 
         return self.text_markup
 
-    def get_epub_section(self, refresh=False):
-        return re.match(Chapter.section_regex, self.get_title(refresh)).group(1)
+    def get_epub_section(self):
+        return re.match(Chapter.section_regex, self.get_title()).group(1)
 
-    def get_epub_filename(self, refresh=False):
-        return self.get_title(refresh).replace(' ', '_') + '.xhtml'
+    def get_epub_filename(self):
+        return self.get_title().replace(' ', '_') + '.xhtml'
 
-    def to_epub(self, css, refresh=False):
-        epub_chapter = epub.EpubHtml(title=self.get_title(refresh), file_name=self.get_epub_filename(refresh), lang='hr')
-        epub_chapter.content='<html><body><h1>'+self.get_title(refresh)+'</h1>'+self.get_text(refresh)+'</body></html>'
+    def to_epub(self, css):
+        epub_chapter = epub.EpubHtml(title=self.get_title(), file_name=self.get_epub_filename(), lang='hr')
+        epub_chapter.content='<html><body><h1>'+self.get_title()+'</h1>'+self.get_text()+'</body></html>'
         epub_chapter.add_item(css)
         return epub_chapter
         
 class TableOfContents:
     toc_entry_class = 'entry-content'
     
-    def __init__(self, url, cache_location='./cache/parahumans'):
+    def __init__(self, url):
         self.url = url
-        self.cache_location = cache_location
         self.tree = None
-        self.chapters = OrderedDict()
+        self.chapters = None
 
     def __getstate__(self):
         odict = self.__dict__.copy()
-        if 'tree' in odict:
-            del odict['tree']
-        return odict
+        odict['tree'] = None
+        return odict       
+ 
+    def load_dom(self):
+        response = urlopen(self.url)
+        self.tree = lxml.html.fromstring(response.read().decode('utf-8', 'ignore'))
+        response.close()
+
+        return self.tree
+
+    def get_chapters(self):
+        if self.tree is not None:
+            chapters = OrderedDict()
+            sel = CSSSelector('div.entry-content a:not([href*="share"])')
+
+            for link in sel(self.tree):
+                href = link.get('href')
+                if not href.startswith('https://'):
+                    href = 'https://' + href
+
+                if href not in chapters:
+                    chapters[href] = Chapter(href)
+            self.chapters = list(chapters.values())
+
+        return self.chapters
+
+class Book:
+    def __init__(self, toc_url, title, author, cache_location='./cache/parahumans'):
+        self.cache_location = cache_location
+        self.toc = TableOfContents(toc_url)
+        self.chapters = None
+        self.title = title
+        self.author = author
 
     @classmethod
     def restore(cls, cache_location='./cache/parahumans'):
         with open(cache_location, 'rb') as f: 
             return pickle.load(f)
-        
-    def load_dom(self, refresh=False):
-        if self.tree is None or refresh:
-            print('loading webpage ' + self.url)
-            response = urlopen(self.url)
-            self.tree = lxml.html.fromstring(response.read().decode('utf-8', 'ignore'))
-            response.close()
-        return self.tree
 
     def cache(self):
         with open(self.cache_location, 'wb') as cache:
             pickle.dump(self, cache)
 
-    def get_chapters(self, refresh=False):
-        if len(self.chapters) == 0 or refresh:
-            sel = CSSSelector('div.entry-content a:not([href*="share"])')
-            for link in sel(self.load_dom()):
-                href = link.get('href')
-                if not href.startswith('https://'):
-                    href = 'https://' + href
-
-                if href not in self.chapters:
-                    self.chapters[href] = Chapter(href)
-
-        return self.chapters.values()        
-
-    def to_epub(self, book, css, refresh=False):
-        sections = OrderedDict()
-        chapters = []
-
-        for chapter in self.get_chapters(refresh):
-            epub_chapter = chapter.to_epub(css)
-            chapters.append(epub_chapter)
-            epub_section = chapter.get_epub_section(refresh)
-
-            if epub_section not in sections:
-                sections[epub_section] = []
-            sections[epub_section].append(epub_chapter)
-
-        book.toc = [(epub.Section(section), tuple(chapters)) for section, chapters in sections.items()]
-
-        return chapters
-
-class Book:
-    def __init__(self, toc_url, title, author):
-        self.toc = TableOfContents(toc_url)
-        self.title = title
-        self.author = author
-        self.init_epub()
+    def init_html(self):
+        print('Scraping table of contents data from website')
+        self.toc.load_dom()
+        self.chapters = self.toc.get_chapters()
+        
+        print('Scraping chapter data from website')
+        for chapter in tqdm(self.chapters):
+            print(chapter.url)
+            chapter.load_dom()
 
     def init_epub(self):
         self.book = epub.EpubBook()
@@ -152,27 +144,51 @@ class Book:
         with open(filename, 'r') as css:
             return epub.EpubItem(uid=uid, file_name="style/"+filename, media_type="text/css", content=css.read())
 
-    def generate_from_web(self, refresh=False):
-        chapters = self.toc.to_epub(self.book, Book.get_css(), refresh)
+    def generate_epub(self):
+        print('Initializing epub')
+        self.init_epub()
 
         #spine is used when navigating forward and backward through the epub
         #first element is 'nav' followed by each epub.EpubHtml chapter in order
         self.book.spine = ['nav']
 
-        for chapter in chapters:
-            self.book.add_item(chapter)
-            self.book.spine.append(chapter)
+        sections = OrderedDict()
 
+        print('Generate chapters')
+        for chapter in tqdm(self.chapters):
+            epub_chapter = chapter.to_epub(Book.get_css())
+
+            self.book.add_item(epub_chapter)
+            self.book.spine.append(epub_chapter)
+
+            epub_section = chapter.get_epub_section()
+
+            if epub_section not in sections:
+                sections[epub_section] = []
+            sections[epub_section].append(epub_chapter)
+
+        print('Generating table of contents')
+        self.book.toc = [(epub.Section(section), tuple(chapters)) for section, chapters in tqdm(sections.items())]
         self.book.add_item(epub.EpubNcx())
         self.book.add_item(epub.EpubNav())
 
+        print('Finished genetaring ebook')
         return self.book
         
 if __name__ == '__main__':
-    book = Book('https://parahumans.wordpress.com/table-of-TableOfContents/', 'Parahumans - Worm', 'Wildbow')
+    book = None
 
-    book.toc.chapters['https://parahumans.wordpress.com/2013/03/05/scourge-19-5/'] = Chapter('https://parahumans.wordpress.com/2013/03/05/scourge-19-5/')
-    book.toc.chapters['https://parahumans.wordpress.com/category/stories-arcs-1-10/arc-3-agitation/3-05/'] = Chapter('https://parahumans.wordpress.com/category/stories-arcs-1-10/arc-3-agitation/3-05/')
+    if os.path.isfile('./cache/parahumans'):
+        book = Book.restore()
+    else:
+        book = Book('https://parahumans.wordpress.com/table-of-contents/', 'Parahumans - Worm', 'Wildbow')
+        book.init_html()
+        book.cache()
 
     # write to the file
-    epub.write_epub('test.epub', book.generate_from_web(), {})
+    epub.write_epub('test.epub', book.generate_epub(), {})
+    '''
+    chapter = Chapter('https://parahumans.wordpress.com/2012/07/26/interlude-12½/')
+    chapter.load_dom()
+    print(chapter.get_text())
+    '''
