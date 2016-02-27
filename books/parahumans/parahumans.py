6168ae54a4738dadb38ab17a3278bd4af5c70fa3
Matthew Stratton <development@mmstratton.com>
2016-02-26 22:08:46 -0500

    restructured config dir

---
+++ books/parahumans/parahumans.py
@@ -0,0 +1,26 @@
+from lib.callbacks import HtmlCallbacks
+import re
+
+class ParahumansHtmlCallbacks(HtmlCallbacks):
+
+    def __init__(self, config):
+        self.config = config
+
+    def chapter_text_callback(self, selector_match):
+        if len(selector_match.cssselect('a')) == 0:
+            return selector_match
+        else:
+            return None
+
+    def chapter_title_callback(self, selector_matches):
+        title = None
+        for match in selector_matches:
+            if match.text is not None:
+                title = match.text
+                break
+        return title
+
+    def chapter_section_callback(self, selector_matches):
+        section_regex = '^(\w+)\s*.*$'
+        title = self.chapter_title_callback(selector_matches)
+        return re.match(section_regex, title).group(1)
